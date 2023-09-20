495b840a6bb98a448a12a14acea8ef68b23c6653
Chris <cablanchette@gmail.com>
2023-09-20 14:24:54 -0400

    Adds files for publishing docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ author = 'Christine Allen-Blanchette, Harsh Sharma, Justice Mason'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = []
 
 source_suffix = {
     ".rst": "restructuredtext",
@@ -28,5 +28,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 html_static_path = ['_static']
