ef4b49bb29536db535de8268c25520f210cf3639
jjmason687 <jjmason@princeton.edu>
2023-09-27 14:11:45 -0400

    Added decoder for SE(3)

--- src/rbnn_gravity/autoencoder.py
+++ src/rbnn_gravity/autoencoder.py
@@ -87,7 +87,14 @@ class EncoderNetSE3(nn.Module):
         Parameters
         ----------
         x : torch.Tensor
-            Input tensor for encoding.
+            Input tensor for encoding -- size (B, obs_len, C, W, H).
+
+        Returns
+        -------
+        x_enc_SO3
+        x_enc_trans
+        indices1
+        indices2
         
         Notes
         -----
@@ -114,3 +121,80 @@ class EncoderNetSE3(nn.Module):
         x_enc_SO3 = self.map_s2s2_so3(z1=z1_enc, z2=z2_enc)
 
         return x_enc_SO3, x_enc_trans, indices2, indices4
+
+class DecoderNetSE3(nn.Module):
+    """
+    Implementation of an deconvolutional decoder network for DEVS to map from the desired latent space to image space.
+    
+    ...
+    
+    """
+    def __init__(self,
+                in_channels: int = 9) -> None:
+        """
+        Parameters
+        ----------
+        in_channels : int
+            Number of channels on the input layer of the encoder network.
+            
+        Notes
+        -----
+        
+        """
+        super().__init__()
+        
+        self.linear7 = nn.Linear(in_features=in_channels, out_features=84)
+        self.relu6 = nn.ELU()
+        self.linear6 = nn.Linear(in_features=84, out_features=120)
+        self.bn5 = nn.BatchNorm1d(120)
+        self.relu5 = nn.ELU()
+        self.linear5 = nn.Linear(in_features=120, out_features=32*4*4)
+        self.unflatten4 = nn.Unflatten(dim=1, unflattened_size=(32, 4, 4))
+        self.bn4 = nn.BatchNorm2d(32)
+        self.maxunpool4 = nn.MaxUnpool2d(2, 2)
+        self.relu4 = nn.ELU()
+        self.conv4 = nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=3)
+        self.relu3 = nn.ELU()
+        self.conv3 = nn.ConvTranspose2d(32, 16, 3)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.maxunpool2 = nn.MaxUnpool2d(2, 2)
+        self.relu2 = nn.ELU()
+        self.conv2 = nn.ConvTranspose2d(in_channels=16, out_channels=16, kernel_size=3)
+        self.relu1 = nn.ELU()
+        self.conv1 = nn.ConvTranspose2d(in_channels=16, out_channels=3, kernel_size=3)
+                
+    def forward(self, x: torch.Tensor, indices2, indices4) -> torch.Tensor:
+        """
+        Compute the encoding for a sequence of images.
+        
+        ...
+        
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input tensor for encoding -- x_enc_SO3 and x_enc_trans concat. 
+            
+        Returns
+        -------
+        x_dec : torch.Tensor
+            Encoded tensor.
+        
+        Notes
+        -----
+        The input should be the concatenation of the transformed x_enc_SO3 (SO3 -> S2S2) and x_enc_trans.
+        
+        """
+        # assert torch.any(x.isnan()) == False and torch.any(x.isinf()) == False
+        x_ = x.reshape(-1, 9)
+        
+        h7 = self.linear7(x_)
+        h6 = self.linear6(self.relu6(h7))
+        h5 = self.linear5(self.relu5(self.bn5(h6)))
+        h4 = self.bn4(self.unflatten4(h5))
+        
+        h4 = self.conv4(self.relu4(self.maxunpool4(h4, indices4)))
+        h3 = self.conv3(self.relu3(h4))
+        h2 = self.bn2(h3)
+        h2 = self.conv2(self.relu2(self.maxunpool2(h2, indices2)))
+        x_dec = self.conv1(self.relu1(h2))
+        return x_dec
