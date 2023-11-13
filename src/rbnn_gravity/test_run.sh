c716065955936f55754dfb451f4b06244b8ff0a9
jjmason687 <jjmason@princeton.edu>
2023-11-12 22:59:12 -0500

    Created test bash script

---
+++ src/rbnn_gravity/test_run.sh
@@ -0,0 +1,18 @@
+CUDA_VISIBLE_DEVICE=0 python src/rbnn_gravity/run.py --exp_name='test_run'\
+                                    --date='11122023'\
+                                    --data_dir='src/rbnn_gravity/data/test_folder/test.npz'\
+                                    --save_dir='src/rbnn_gravity/test_models/'\
+                                    --gpu_id=0\
+                                    --seed=0\
+                                    --n_epochs=10\
+                                    --lr=1e-3\
+                                    --dt=1e-3\
+                                    --tau=2\
+                                    --V_in_dim=9\
+                                    --V_hidden_dim=50\
+                                    --V_out_dim=1\
+                                    --lambda_loss 1. 1.\
+                                    --print_every=1\
+                                    --retrain_model\
+                                    --save_model\
+
