1cac20db3645b78d663e89452799c3c0aba1f2dc
jjmason687 <jjmason@princeton.edu>
2023-12-17 13:48:09 -0500

    Updated the dataset template

--- src/rbnn_gravity/data/bash_scripts/dataset_template.sh
+++ src/rbnn_gravity/data/bash_scripts/dataset_template.sh
@@ -1,13 +1,14 @@
-CUDA_VISIBLE_DEVICE=0 python src/rbnn_gravity/data/generate_data.py --name='exp-1'\
-                                                                --date='111323'\
+CUDA_VISIBLE_DEVICE=0 python src/rbnn_gravity/data/generate_data.py --name='exp-uniform'\
+                                                                --date='120323'\
                                                                 --save_dir='src/rbnn_gravity/data/generated_datasets'\
-                                                                --n_examples=100\
+                                                                --n_examples=1000\
                                                                 --traj_len=100\
                                                                 --dt=0.001\
                                                                 --mass=1.\
                                                                 --radius=50.\
-                                                                --ic_type='random'\
+                                                                --R_ic_type='uniform'\
+                                                                --pi_ic_type='random'\
                                                                 --seed=0\
-                                                                --moi_diag_gt 3. 2. 1.\
+                                                                --moi_diag_gt 1. 2.8 2.\
                                                                 --moi_off_diag_gt 0. 0. 0.\
                                                 
